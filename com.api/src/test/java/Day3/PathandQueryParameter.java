1a846dadd9c32fa719c8067589bbe075479e68c1
shubham <shubhamagalgave11111@gmail.com>
2023-08-16 19:32:42 +0530

    save path parameter

---
+++ com.api/src/test/java/Day3/PathandQueryParameter.java
@@ -0,0 +1,36 @@
+package Day3;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+
+
+import java.util.HashMap;
+
+import org.testng.annotations.Test;
+
+public class PathandQueryParameter {
+	
+	    @Test(priority=1)
+		public void postRequest()
+		{
+			given()
+			.pathParam("user", "users")
+			.queryParam("page", "2")
+			.queryParam("id", "5")
+		    
+		    
+		    .when().get("https://reqres.in/api/{user}").
+		   
+		
+		    then()
+		    .statusCode(200)           
+		  // .body("id",equalTo(5))
+		  // .body("first_name",equalTo("Charles"))  
+		    .log().all();             
+		}
+
+
+}
