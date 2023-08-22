3a4efb2551d434a31436997acf52c66c6dcbfcc6
shubham <shubhamagalgave11111@gmail.com>
2023-08-22 16:30:24 +0530

    fjvnfkdvf

--- com.api/src/test/java/Day8/putRequest.java
+++ com.api/src/test/java/Day8/putRequest.java
@@ -8,8 +8,8 @@ import org.testng.annotations.Test;
 public class putRequest{
 	
 	  @Test
-			public void putRequest(ITestContext context)
-			{
+			public void putRequest(ITestContext context){
+		  
 			   String Token="bc5549ea7c37490b8d487ecfbe48f516bb863d02fc56fdb7d80214552a36fee7";
 			   int id=(Integer)context.getAttribute("userId");
 			   given()
